be02ee56fb05f2556dd73cf5ce35469a4e51fdb0
Petrik Lajos Szg <info@petrik.hu>
2018-09-24 15:36:15 +0200

    1,2,3 feladat kész

--- Feladat01/Program.cs
+++ Feladat01/Program.cs
@@ -10,6 +10,66 @@ namespace Feladat01
     {
         static void Main(string[] args)
         {
+
+            //1.Feladat
+
+            var szamok = new List<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Adj meg egy szamot");
+                string userSzam = Console.ReadLine();
+
+                szamok[i] = int.Parse(userSzam);
+            }
+
+            int max = szamok[0];
+
+            foreach (var e in szamok)
+            {
+                if(e > max)
+                {
+                    max = e;
+                }
+            }
+
+            Console.WriteLine("A legnagyobb szám amit megadtál: " + max);
+
+            Console.WriteLine("Kérem adja meg, hogy hány számot szeretne megadni: ");
+            String n = Console.ReadLine();
+
+            int darab = int.Parse(n);
+
+            int[] tomb = new int[darab];
+
+            for(int i = 0; i < darab; i++)
+            {
+                Console.WriteLine("Adj meg egy szamot: ");
+                string userSzam = Console.ReadLine();
+
+                tomb[i] = int.Parse(userSzam);
+            }
+
+            int max2 = tomb[0];
+            int min = tomb[0];
+
+            foreach (var e in tomb)
+            {
+                if (e > max2)
+                {
+                    max2 = e;
+                }
+
+                if (min > e)
+                {
+                    min = e;
+                }
+            }
+
+            Console.WriteLine("A legnagyobb szám amit megadtál: " + max2);
+
+            Console.WriteLine("A legkisebb szám amit megadtál: " + min);
+
         }
     }
 }
